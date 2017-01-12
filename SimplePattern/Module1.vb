23990ef9ca730d956ab6591f552a4492c07ada7e
jovanidash21 <jwardash21@gmail.com>
2017-01-12 16:31:11 +0800

    Add Pattern Function

--- SimplePattern/Module1.vb
+++ SimplePattern/Module1.vb
@@ -1,7 +1,64 @@
 ﻿Module Module1
 
     Sub Main()
+        Dim side As Integer = 5
+        Dim row As Integer
+        Dim column As Integer
 
+        Console.WriteLine("Pattern 1")
+        For row = 1 To side
+            For column = side To 1 Step -1
+                If (row = column) Then
+                    Console.Write("-")
+                Else
+                    Console.Write("0")
+                End If
+            Next
+            Console.WriteLine()
+        Next
+        Console.WriteLine()
+
+        Console.WriteLine("Pattern 2")
+        For row = 1 To side
+            For column = 1 To side
+                If (row = column) Then
+                    Console.Write("-")
+                Else
+                    Console.Write("0")
+                End If
+            Next
+            Console.WriteLine()
+        Next
+        Console.WriteLine()
+
+        Console.WriteLine("Pattern 3")
+        For row = 1 To side
+            For column = 1 To side
+                If (column Mod 2 = 0) Then
+                    Console.Write("-")
+                Else
+                    Console.Write("0")
+                End If
+            Next
+            Console.WriteLine()
+        Next
+        Console.WriteLine()
+
+        Console.WriteLine("Pattern 4")
+        For row = 1 To side
+            If row Mod 2 = 0 Then
+                For column = 1 To side
+                    Console.Write("-")
+                Next
+            Else
+                For c = 1 To side
+                    Console.Write("0")
+                Next
+            End If
+            Console.WriteLine()
+        Next
+
+        Console.ReadLine()
     End Sub
 
 End Module
